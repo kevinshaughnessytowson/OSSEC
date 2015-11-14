122e3423b058ba29cc376f8213373024876bea70
kevinshaughnessytowson <kshaug2@students.towson.edu>
2015-11-14 00:33:42 -0500

    added ports

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/bin/python
 
 import socks
-
+#dictionary of top ports as keys and associated services as values
+top_ports = {7:"Echo", 20:"FTP Data", 21:"FTP Service", 22:"SSH", 23:"Telnet", 25:"SMTP", 42:"WINS", 43:"WHOIS", 53:"DNS", 67:"DHCP", 68:"DHCP", 69:"TFTP", 80:"HTTP", 88:"Kerberos", 110:"POP3", 123:"NTP", 135:"RPC", 137:"NetBIOS", 138:"NetBIOS", 139:"NetBIOS", 161:"SNMP", 162:"SNMP", 389:"LDAP", 443:"SSL", 445:"Microsoft DS", 513:"rlogin", 514:"syslog", 520:"RIP", 546:"DHCPv6", 547:"DHCPv6", 587:"SMTP", 636:"LDAP over SSL", 989:"FTP over SSL", 990:"FTP over SSL", 993:"IMAP4 over SSL", 995:"POP3 over SSL", 1194:"OpenVPN", 1080:"SOCKS Proxy", 1241:"Nessus", 1812:"RADIUS", 1813:"RADIUS", 2483:"Oracle DB", 
 #set proxy for python to use, this will be a tor proxy so tor must be setup for it to work
-target_socket = socks.socksocket()
-s.setproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050)
+def main():
+	target_socket = socks.socksocket()
+	s.setproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 9050)
