87807e93c16d86d8cc3b7611d7c5905c9a72894c
kevinshaughnessytowson <kshaug2@students.towson.edu>
2015-11-14 21:04:48 -0500

    fixed errors in db script

--- setUpDB.py
+++ setUpDB.py
@@ -1,22 +1,21 @@
 import MySQLdb as sql
 
-db = sql.connect(host="localhost", user="root", passwd="toor")
+db = sql.connect(host="localhost", user="root", passwd="toor", db="scan")
 cursor = db.cursor()
-create_stmt = 'CREATE DATABASE scan; CREATE TABLE `services` (" \
-    "  `port` int(8) NOT NULL," \
-    "  `packet_format` text NOT NULL," \
-    "  PRIMARY KEY (`port`)" \
-    ") ENGINE=InnoDB'
+create_stmt = 'CREATE DATABASE scan;'
 
 try:
 	cursor.execute(create_stmt)
 except:
 	print("Database already exists")
 
-db.database = "scan"
+try:
+	cursor.execute('CREATE TABLE `services` (`port` int(8) NOT NULL,`packet_format` text NOT NULL, PRIMARY KEY (`port`)) ENGINE=InnoDB')
+except:
+	print("Table already exists")
 
-cursor.execute('INSERT INTO services (`port`, `packet_format`) VALUES (`7`, `sr1(IP(dst=?)/ICMP())`)')
-cursor.execute('INSERT INTO services (`port`, `packet_format`) VALUES (`21`, `sr1(IP(dst=?)/ICMP())`)')
+cursor.execute('INSERT INTO services (`port`, `packet_format`) VALUES (7, "sr1(IP(dst=?)/ICMP())")')
+cursor.execute('INSERT INTO services (`port`, `packet_format`) VALUES (21, "sr1(IP(dst=?)/ICMP())")')
 #create_table = ("CREATE TABLE `services` ("
 #    "  `port` int(8) NOT NULL,"
 #    "  `packet_format` text NOT NULL,"
